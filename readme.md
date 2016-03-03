9ac07f6da1490f3293f9121c21e96996d7d24e15
Dumitru Cantea <canteadumitru@gmail.com>
2016-03-03 17:50:54 +0200

    added yr.no api

--- readme.md
+++ readme.md
@@ -1,10 +1,35 @@
 # yrno nodejs client
 
+## Usage
+```
+var client = require('yrno-client');
+client.getWeather({
+  params: {lat: 20.123, lon: 43.232},
+  request: {timeout: 1000}
+}, function(error, report){
+  // got weather report
+});
+```
 
-## Output
+## API
 
-Result is a json object.
+### .api.ENDPOINT(options, callback)
 
+ny.no api endpoint call.
+
+### .getWeather(options, callback)
+
+Returns a weahter report
+
+- **options**:
+  - **params**:
+    - **lat** - latitude,
+    - **lon** - longitude
+  - *version* - api version
+  - *request* - request options
+- **callback** - function(error, data)
+
+Data example:
 ```
 { created: '2016-03-02T20:26:35Z',
   meta: { model: [ [Object], [Object] ] },
--- test/client.js
+++ test/client.js
@@ -6,8 +6,10 @@ var client = require('../lib/client');
 describe('Client', function() {
 	it('should get report data with params', function(done) {
 		client.getWeather({
-			lat: 53.3478,
-			lon: 6.2597
+			params: {
+				lat: 53.3478,
+				lon: 6.2597
+			}
 		}, function(error, data) {
 			assert.ok(data);
 			done();
@@ -15,18 +17,25 @@ describe('Client', function() {
 	});
 	it('should get report data with params & options', function(done) {
 		client.getWeather({
-			lat: 53.3478,
-			lon: 6.2597
-		}, { timeout: 5000 }, function(error, data) {
+			params: {
+				lat: 53.3478,
+				lon: 6.2597
+			},
+			request: { timeout: 6000 }
+		}, function(error, data) {
 			assert.ok(data);
 			done();
 		});
 	});
 	it('should get report data with params, options & version', function(done) {
 		client.getWeather({
-			lat: 53.3478,
-			lon: 6.2597
-		}, { timeout: 5000 }, 1.9, function(error, data) {
+			params: {
+				lat: 53.3478,
+				lon: 6.2597
+			},
+			request: { timeout: 4000 },
+			version: 1.9
+		}, function(error, data) {
 			assert.ok(data);
 			done();
 		});
